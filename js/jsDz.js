03c070b78d07b2d0326571f761ca0dac3dab3d3d
Aleksandr <kacchklim3@gmail.com>
2022-01-17 14:16:02 +0200

    dz function

--- js/jsDz.js
+++ js/jsDz.js
@@ -16,11 +16,46 @@ function isPrime(num) {
 
   console.log(isPrime(4));
 
-  function checkMultiplicity(n1,n2) {
+function checkMultiplicity(n1,n2) {
       return n1%n2 == 0;
-  }
+}
 
-  console.log(checkMultiplicity(25, 5));
-  console.log(checkMultiplicity(15, 3));
-  console.log(checkMultiplicity(15, 5));
-  console.log(checkMultiplicity(15, 4));
+console.log(checkMultiplicity(25, 5));
+console.log(checkMultiplicity(15, 3));
+console.log(checkMultiplicity(15, 5));
+console.log(checkMultiplicity(15, 4));
+
+/* function triangle(a,b,c) {
+  {
+    return (a + b > c && a + c > b && b + c >a);
+  } 
+}
+
+console.log(triangle(25,25,50));
+console.log(triangle(23,25,25));
+
+function areaOfTriangle(base, height) {
+  return 0.5 * base * height;
+}
+
+console.log(areaOfTriangle(10,20)); */
+
+
+function areaOfRectangle(a1,b1) {
+  return a1 * b1;
+}
+
+console.log(areaOfRectangle(15,12));
+
+const triangle = function (a,b,c) {
+    return (a + b > c && a + c > b && b + c >a);
+  } 
+
+console.log(triangle(25,25,50));
+console.log(triangle(23,25,25));
+
+const areaOfTriangle = function (base, height) {
+  return 0.5 * base * height;
+}
+
+console.log(areaOfTriangle(10,20));
